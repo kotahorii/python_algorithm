043ceb7e7084872d167af35a05c44276c3ded673
kotahorii <khorii.micron@gmail.com>
2022-02-12 15:13:17 +0900

    initial commit

--- chapter2.py
+++ chapter2.py
@@ -41,14 +41,30 @@
 
 #     print(calc_results)
 
-for devidend in range(2, 101):
-    max_range = devidend // 2
-    is_prime_number = True
+# for devidend in range(2, 101):
+#     max_range = devidend // 2
+#     is_prime_number = True
 
-    for devisor in range(2, max_range + 1):
-        if devidend % devisor == 0:
-            is_prime_number = False
-            break
+#     for devisor in range(2, max_range + 1):
+#         if devidend % devisor == 0:
+#             is_prime_number = False
+#             break
 
-    if is_prime_number:
-        print(devidend)
+#     if is_prime_number:
+#         print(devidend)
+
+# factorial_num = 1
+# for i in range(1, 11):
+#     factorial_num *= i
+
+# print(factorial_num)
+
+
+def culc_factorial(n: int) -> int:
+    if n == 0:
+        return 1
+    return n * culc_factorial(n - 1)
+
+
+for i in range(21):
+    print(culc_factorial(i))
