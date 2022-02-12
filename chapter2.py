babeabbff2b93e7c00d749504f2932aad2c9e0e5
kotahorii <khorii.micron@gmail.com>
2022-02-12 14:05:44 +0900

    initial commit

--- chapter2.py
+++ chapter2.py
@@ -1,9 +1,35 @@
-subject_score = [70, 98, 92, 88, 64]
-total_score = 0
-average_score = 0
+# subject_score = [70, 98, 92, 88, 64]
+# total_score = 0
+# average_score = 0
 
-for i, _ in enumerate(subject_score):
-    total_score += subject_score[i]
-    average_score = total_score / len(subject_score)
+# for i, _ in enumerate(subject_score):
+#     total_score += subject_score[i]
+#     average_score = total_score / len(subject_score)
 
-print(total_score, average_score)
+# print(total_score, average_score)
+
+# sum = 0
+# for i in range(1, 11):
+#     sum += i
+
+# print(sum)
+
+
+# def sum_number(n: int) -> int:
+#     total = 0
+
+#     for i in range(1, n + 1):
+#         total += i
+
+#     return total
+
+
+# print(sum_number(10))
+
+
+def sum_number(n: int) -> int:
+    total = (1 + n) * (n / 2)
+    return int(total)
+
+
+print(sum_number(9))
