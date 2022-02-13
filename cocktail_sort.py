8d82cbbe16a8de0b5f0787521ac23b15e367cd83
kotahorii <khorii.micron@gmail.com>
2022-02-13 19:52:09 +0900

    initial commit

---
+++ cocktail_sort.py
@@ -0,0 +1,31 @@
+import random
+
+
+def cocktail_sort(numbers: list[int]) -> list[int]:
+    swapped = True
+    start = 0
+    end = len(numbers) - 1
+
+    while swapped:
+        swapped = False
+        for i in range(start, end):
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+                swapped = True
+
+        if not swapped:
+            break
+        swapped = False
+        end -= 1
+
+        for i in range(end - 1, start - 1, -1):
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+                swapped = True
+        start += 1
+    return numbers
+
+
+if __name__ == "__main__":
+    nums = [random.randint(0, 1000) for _ in range(10)]
+    print(cocktail_sort(nums))
