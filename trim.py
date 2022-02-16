d298b811892df33b2498e2ef296072e40600a3af
kotahorii <khorii.micron@gmail.com>
2022-02-16 14:15:47 +0900

    initial commit

---
+++ trim.py
@@ -0,0 +1,32 @@
+l: list[int] = [1, 3, 3, 5, 5, 7, 7, 7, 10, 12, 12, 15]
+
+
+def delete_duplicate_v1(numbers: list[int]) -> None:
+    tmp = []
+    for num in numbers:
+        if num not in tmp:
+            tmp.append(num)
+    numbers[:] = tmp
+
+
+def delete_duplicate_v2(numbers: list[int]) -> None:
+    tmp = [numbers[0]]
+    i, len_num = 0, len(numbers) - 1
+    while i < len_num:
+        if numbers[i] != numbers[i + 1]:
+            tmp.append(numbers[i + 1])
+        i += 1
+    numbers[:] = tmp
+
+
+def delete_duplicate_v4(numbers: list[int]) -> None:
+    i = len(numbers) - 1
+    while i > 0:
+        if numbers[i] == numbers[i - 1]:
+            numbers.pop(i)
+        i -= 1
+
+
+if __name__ == "__main__":
+    delete_duplicate_v4(l)
+    print(l)
