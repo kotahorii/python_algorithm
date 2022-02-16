f7a2a0ba1436f93967e60d3601be8cc9cca11750
kotahorii <khorii.micron@gmail.com>
2022-02-16 14:52:39 +0900

    initial commit

---
+++ even_odd.py
@@ -0,0 +1,14 @@
+def order_even_first_odd_last_v2(numbers: list[int]) -> None:
+    i, j = 0, len(numbers) - 1
+    while i < j:
+        if numbers[i] % 2 == 0:
+            i += 1
+        else:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            j -= 1
+
+
+if __name__ == "__main__":
+    l = [0, 1, 3, 4, 2, 4, 5, 1, 6, 9, 8]
+    order_even_first_odd_last_v2(l)
+    print(l)
